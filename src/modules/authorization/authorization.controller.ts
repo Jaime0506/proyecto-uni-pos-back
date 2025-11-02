4466ccea686841acbdcabcb99956f8038b50b10f
Jaime Mejia <jaimemejia1003@gmail.com>
2025-11-01 19:02:31 -0500

    Enhance AuthorizationController by adding @HttpCode(200) decorators to all role and permission endpoints for consistent HTTP response status handling.

--- src/modules/authorization/authorization.controller.ts
+++ src/modules/authorization/authorization.controller.ts
@@ -3,6 +3,7 @@ import {
 	Controller,
 	Delete,
 	Get,
+	HttpCode,
 	Param,
 	ParseIntPipe,
 	Patch,
@@ -22,52 +23,62 @@ export class AuthorizationController {
 
 	// Permissions
 	@Get('permissions/get-all')
+	@HttpCode(200)
 	async getAllPermissions() {
 		return this.authorizationService.getAllPermissions();
 	}
 
 	@Get('permissions/get-by-id/:id')
+	@HttpCode(200)
 	async getPermissionById(@Param('id', ParseIntPipe) id: number) {
 		return this.authorizationService.getPermissionById(id);
 	}
 
 	@Post('permissions/create')
+	@HttpCode(200)
 	async createPermission(@Body() dto: CreatePermissionDto) {
 		return this.authorizationService.createPermission(dto);
 	}
 
 	@Patch('permissions/update')
+	@HttpCode(200)
 	async updatePermission(@Body() dto: UpdatePermissionDto) {
 		return this.authorizationService.updatePermission(dto);
 	}
 
 	@Delete('permissions/delete')
+	@HttpCode(200)
 	async deletePermission(@Body() dto: DeletePermissionDto) {
 		return this.authorizationService.deletePermission(dto);
 	}
 
 	// Roles
 	@Get('roles/get-all/:id_company')
+	@HttpCode(200)
 	async getAllRoles(@Param('id_company', ParseIntPipe) id_company: number) {
 		return await this.authorizationService.getAllRoles(id_company);
 	}
 
 	@Get('roles/get-by-id/:id')
+	@HttpCode(200)
 	async getRoleById(@Param('id', ParseIntPipe) id: number) {
 		return await this.authorizationService.getRoleById(id);
 	}
 
 	@Post('roles/create')
+	@HttpCode(200)
 	async createRole(@Body() dto: CreateRoleDto) {
 		return await this.authorizationService.createRole(dto);
 	}
 
 	@Patch('roles/update')
+	@HttpCode(200)
 	async updateRole(@Body() dto: UpdateRoleDto) {
 		return await this.authorizationService.updateRole(dto);
 	}
 
 	@Delete('roles/delete')
+	@HttpCode(200)
 	async deleteRole(@Body() dto: DeleteRoleDto) {
 		return await this.authorizationService.deleteRole(dto);
 	}
